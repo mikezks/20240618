30fc40c9635cf084e02d70dbb71a22629f7d92ba
mikezks <23002661+mikezks@users.noreply.github.com>
2024-06-18 16:30:04 +0200

    feat: auth service

---
+++ projects/flight/src/app/shared/logic-auth/auth-service/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isLoggedIn = new BehaviorSubject<boolean>(true);
+}
