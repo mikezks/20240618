a971f24a3439985e36455927eced4581d109c7c0
mikezks <23002661+mikezks@users.noreply.github.com>
2024-06-18 16:29:18 +0200

    feat: inject, injection context

--- projects/flight/src/app/boarding/feature-departure/flight-departure/departure.component.ts
+++ projects/flight/src/app/boarding/feature-departure/flight-departure/departure.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Injector, OnInit, inject, runInInjectionContext } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, catchError, debounceTime, distinctUntilChanged, filter, of, switchMap, tap } from 'rxjs';
 import { Flight, FlightService } from '../../../booking/api-boarding';
+import { injectApiUrl } from '../../../shared/util-config';
 
 
 @Component({
   selector: 'app-departure',
   templateUrl: './departure.component.html'
 })
-export class DepatureComponent {
+export class DepatureComponent implements OnInit {
+  private flightService = inject(FlightService);
+  private injector = inject(Injector);
+
   control = new FormControl('', { nonNullable: true });
   flights$ = this.initFlightsStream();
   loading = false;
 
-  constructor(private flightService: FlightService) {}
+  ngOnInit(): void {
+    runInInjectionContext(
+      this.injector,
+      () => inject(FlightService)
+    );
+
+    runInInjectionContext(
+      this.injector,
+      () => injectApiUrl()
+    );
+  }
 
   initFlightsStream(): Observable<Flight[]> {
     return this.control.valueChanges.pipe(
