f1bc842914c149300c4f897b5776d6c22728d275
mikezks <23002661+mikezks@users.noreply.github.com>
2024-06-18 13:40:11 +0200

    refactor: feature routes, default export

--- projects/flight/src/app/booking/booking.routes.ts
+++ projects/flight/src/app/booking/booking.routes.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FlightBookingComponent, FlightEditComponent, FlightSearchComponent } from './feature-flight';
 import { FlightResolver } from './logic-flight';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { TicketEffects } from './logic-flight/+state/effects';
+import { ticketFeature } from './logic-flight/+state/reducer';
 
 
-const routes: Routes = [
+export const BOOKING_ROUTES: Routes = [
   {
     path: '',
+    providers: [
+      importProvidersFrom(
+        StoreModule.forFeature(ticketFeature),
+        EffectsModule.forFeature([TicketEffects]),
+      )
+    ],
     component: FlightBookingComponent,
     children: [
       {
@@ -38,9 +48,3 @@ const routes: Routes = [
     ]
   }
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class BookingRoutingModule { }
---
+++ projects/flight/src/app/booking/index.ts
@@ -0,0 +1,5 @@
+
+import * as routes from './booking.routes';
+
+export const BOOKING_ROUTES = routes.BOOKING_ROUTES;
+export default BOOKING_ROUTES;
